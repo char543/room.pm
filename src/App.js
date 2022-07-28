207e173f5b74d2766116f59eaab7511ffe95cb0e
char543 <charlie.stuart92@gmail.com>
2022-07-28 19:55:36 +0100

    sumo, tailwind, meta tags

--- src/App.js
+++ src/App.js
@@ -5,56 +5,56 @@ function App() {
 
 //setup useState for mailer
 
-const [mailerState, setMailerState] = useState({
-  email: "",
-  name: "",
-  message: "",
-});
+// const [mailerState, setMailerState] = useState({
+//   email: "",
+//   name: "",
+//   message: "",
+// });
 
 //function to handle the state
 
-function handleStateChange(e) {
-  setMailerState((prevState) => ({
-    ...prevState,
-    [e.target.name]: e.target.value,
-  }));
-}
+// function handleStateChange(e) {
+//   setMailerState((prevState) => ({
+//     ...prevState,
+//     [e.target.name]: e.target.value,
+//   }));
+// }
 
 //function to post to the route
 
-const submitEmail = async (e) => {
-e.preventDefault();
-console.log({ mailerState });
-const response = await fetch("http://localhost:3001/send", {
-  method: "POST",
-  headers: {
-    "Content-type": "application/json",
-  },
-  body: JSON.stringify({ mailerState }),
-})
-  .then((res) => res.json())
-  .then(async (res) => {
-    const resData = await res;
-    console.log(resData);
-    if (resData.status === "success") {
-      alert("Message Sent");
-    } else if (resData.status === "fail") {
-      alert("Message failed to send");
-    }
-  })
-  .then(() => {
-    setMailerState({
-      email: "",
-      name: "",
-      message: "",
-    });
-  });
-};
+// const submitEmail = async (e) => {
+// e.preventDefault();
+// console.log({ mailerState });
+// const response = await fetch("http://localhost:3001/send", {
+//   method: "POST",
+//   headers: {
+//     "Content-type": "application/json",
+//   },
+//   body: JSON.stringify({ mailerState }),
+// })
+//   .then((res) => res.json())
+//   .then(async (res) => {
+//     const resData = await res;
+//     console.log(resData);
+//     if (resData.status === "success") {
+//       alert("Message Sent");
+//     } else if (resData.status === "fail") {
+//       alert("Message failed to send");
+//     }
+//   })
+//   .then(() => {
+//     setMailerState({
+//       email: "",
+//       name: "",
+//       message: "",
+//     });
+//   });
+// };
 
   return (
     <div className="App h-screen bg-white justify-around items-center flex flex-col">
       <img className='p-12' src='room.jpg'></img>
-      <div className="z-10 flex justify-center items-center flex-col text-black">
+      {/* <div className="z-10 flex justify-center items-center flex-col text-black">
           <span className="footer-title">Newsletter</span>
           <div className="form-control w-[280px] md:w-80">
             <label className="label">
@@ -73,7 +73,8 @@ const response = await fetch("http://localhost:3001/send", {
               </button>
             </form>
           </div>
-        </div>
+        </div> */}
+        <span className='hidden' data-sumome-listbuilder-embed-id="68c78aa33c2db037ade6454325cf5c658ee41a301485a360b828f79bca5d8e8a"></span>
     </div>
   );
 }

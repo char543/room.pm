ca1d66d200158e11bf855cb030603cfa9c646370
char543 <36009210+char543@users.noreply.github.com>
2022-09-18 22:41:03 +0100

    install daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,jsx}', 'node_modules/daisyui/dist/**/*.js'],
-  theme: {
-    screens: {
-      sm: '640px',
-      // => @media (min-width: 640px) { ... }
+	content: ["./src/**/*.{html,js,jsx}", "node_modules/daisyui/dist/**/*.js"],
+	theme: {
+		screens: {
+			sm: "640px",
+			// => @media (min-width: 640px) { ... }
 
-      md: '768px',
-      // => @media (min-width: 768px) { ... }
+			md: "768px",
+			// => @media (min-width: 768px) { ... }
 
-      lg: '1024px',
-      // => @media (min-width: 1024px) { ... }
+			lg: "1024px",
+			// => @media (min-width: 1024px) { ... }
 
-      xl: '1280px',
-      // => @media (min-width: 1280px) { ... }
-    },
-    fontFamily: {
-      sans: ['Poppins'],
-    },
-    extend: {
-      colors: {
-        navGrey: 'rgb(42, 48, 60)',
-      },
-    },
-  },
-  plugins: [require("daisyui")],
-}
+			xl: "1280px",
+			// => @media (min-width: 1280px) { ... }
+		},
+		fontFamily: {
+			sans: ["Poppins"],
+		},
+		extend: {
+			colors: {
+				navGrey: "rgb(42, 48, 60)",
+			},
+		},
+	},
+	plugins: [require("daisyui")],
+};
